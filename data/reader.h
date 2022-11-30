f514a3dddf922bbf8fcdcd44b37a1a1d178212a0
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2022-11-30 17:02:00 -0600

    Update reader.h

--- data/reader.h
+++ data/reader.h
@@ -1,16 +1,20 @@
 #pragma once
 
-#include <vector>
+// TODO
+// query multiple parameters at once (just make start_idx a parameter)
+
+#include <optional>
 #include <string>
+#include <list>
 
 class Reader {
     public:
         struct RoadEntry {
             std::string name;
             
-            unsigned int length;
+            double length;
 
-            std::vector<std::pair<double, double>> coordinates_list;
+            std::list<std::pair<double, double>> coordinates_list;
         };
 
         struct TrafficEntry {
@@ -18,18 +22,22 @@ class Reader {
             
             unsigned int traffic;
             
-            std::vector<std::pair<double, double>> coordinates_list;
+            std::list<std::pair<double, double>> coordinates_list;
         };
 
         struct CrashEntry {
+            unsigned int vehicles;
+
             std::pair<double, double> coordinates;
         };
 
-        std::vector<RoadEntry> getRoadEntries();
-        std::vector<TrafficEntry> getTrafficEntries();
-        std::vector<CrashEntry> getCrashEntries();
+        std::list<RoadEntry> getRoadEntries();
+        std::list<TrafficEntry> getTrafficEntries();
+        std::list<CrashEntry> getCrashEntries();
 
     private:
-        std::string findValue(std::string parameter);
-
+        std::optional<std::string> findString(std::string line, std::string parameter, bool space);
+        std::optional<int> findInt(std::string line, std::string parameter, bool space);
+        std::optional<double> findDouble(std::string line, std::string parameter, bool space);
+        std::optional<std::list<std::pair<double, double>>> findCoordinatesList(std::string line, std::string parameter, unsigned int dim, bool space);
 };
