3b111ed4c39092080857a16240c7a8d869e88d3c
Anuj Patel <anuj-patel@outlook.com>
2022-11-30 18:42:22 -0600

    Initial image tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,30 @@
+#include "../visuals/image.h"
+
+#include <iostream>
+#include <stdexcept>
+
+void ImageTests() {
+    std::cout << "-> [Test 1]" << std::endl;
+    Image image = Image(50, 50);
+    std::tuple<unsigned int, unsigned int, unsigned int> pixel = image.getPixel(49, 49);
+    if (std::get<0>(pixel) != 0) throw std::runtime_error("Invalid image initialization.");
+    if (std::get<1>(pixel) != 0) throw std::runtime_error("Invalid image initialization.");
+    if (std::get<2>(pixel) != 0) throw std::runtime_error("Invalid image initialization.");
+
+    image.setPixel(49, 49, {255, 255, 255});
+    pixel = image.getPixel(49, 49);
+    if (std::get<0>(pixel) != 255) throw std::runtime_error("Invalid pixel manipulation.");
+    if (std::get<1>(pixel) != 255) throw std::runtime_error("Invalid pixel manipulation.");
+    if (std::get<2>(pixel) != 255) throw std::runtime_error("Invalid pixel manipulation.");
+
+    image.toPPM("tests/test_image_1.ppm");
+}
+
 int main() {
-    return 1;
+    std::cout << "=======================" << std::endl;
+    std::cout << "Starting Image tests..." << std::endl;
+    ImageTests();
+    std::cout << "Completed Image tests." << std::endl;
+    std::cout << "=======================" << std::endl;
+    return 0;
 }
