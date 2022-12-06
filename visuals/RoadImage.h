a687ca290d7dd47b2f611ffccca8258131930fd5
Anuj Patel <91538343+anuj-p@users.noreply.github.com>
2022-12-06 11:55:02 -0600

    Improve Image efficiency

--- visuals/RoadImage.h
+++ visuals/RoadImage.h
@@ -8,10 +8,10 @@
 class RoadImage {
     public:
         RoadImage();
-        void addPath(std::list<std::pair<double, double>> path, std::tuple<unsigned int, unsigned int, unsigned int> color);
-        void addPath(Reader::RoadEntry road, std::tuple<unsigned int, unsigned int, unsigned int> color);
-        void addPaths(std::list<std::list<std::pair<double, double>>> paths, std::tuple<unsigned int, unsigned int, unsigned int> color);
-        void addPaths(std::list<Reader::RoadEntry> roads, std::tuple<unsigned int, unsigned int, unsigned int> color);
+        void addPath(std::list<std::pair<double, double>> path, std::tuple<uint8_t, uint8_t, uint8_t> color);
+        void addPath(Reader::RoadEntry road, std::tuple<uint8_t, uint8_t, uint8_t> color);
+        void addPaths(std::list<std::list<std::pair<double, double>>> paths, std::tuple<uint8_t, uint8_t, uint8_t> color);
+        void addPaths(std::list<Reader::RoadEntry> roads, std::tuple<uint8_t, uint8_t, uint8_t> color);
         void toPNG(const std::string& file_name);
     private:
         Image image;
