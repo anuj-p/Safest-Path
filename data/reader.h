5fb321c6a6331bf56465d69906eb8bf4cd31e8bd
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2022-12-12 11:38:47 -0600

    Added documentation

--- data/reader.h
+++ data/reader.h
@@ -10,15 +10,73 @@
 #include <list>
 #include <tuple>
 
+/**
+ * Reader class
+*/
 class Reader {
     public:
+        /**
+         * A static function that returns a list of RoadEntrys from the file located at "filename"
+         * 
+         * @param filename A string storing the address of the dataset to be read
+         * @return A list of RoadEntrys storing the roads in "filename"
+        */
         static std::list<ReaderUtils::RoadEntry> getRoadEntries(const std::string& filename);
+
+        /**
+         * A static function that returns a list of TrafficEntrys from the file located at "filename"
+         * 
+         * @param filename A string storing the address of the dataset to be read
+         * @return A list of TrafficEntrys storing the traffic frequencies of roads in "filename"
+        */
         static std::list<ReaderUtils::TrafficEntry> getTrafficEntries(const std::string& filename);
+
+        /**
+         * A static function that rteurns a list of CrashEntrys from the file located at "filename"
+         * 
+         * @param filename A string storing the address of the dataset to be read
+         * @return A list of CrashEntrys storing crashes listed in "filename"
+        */
         static std::list<ReaderUtils::CrashEntry> getCrashEntries(const std::string& filename);
 
     private:
+        /**
+         * A static function that finds the JSON string data corresponding to key "parameter" in "line"
+         * 
+         * @param line A string storing the line to be read
+         * @param parameter A string storing the key to be looked up
+         * @param space A bool indicating if there is spacing in the dataset that needs to be adjusted for
+         * @return An optional of string that stores the string of interest
+        */
         static std::optional<std::string> findString(std::string line, std::string parameter, bool space);
+
+        /**
+         * A static function that finds the JSON int data corresponding to key "parameter" in "line"
+         * 
+         * @param line A string storing the line to be read
+         * @param parameter A string storing the key to be looked up
+         * @param space A bool indicating if there is spacing in the dataset that needs to be adjusted for
+         * @return An optional of int that stores the integer of interest
+        */
         static std::optional<int> findInt(std::string line, std::string parameter, bool space);
+
+        /**
+         * A static function that finds the JSON double data corresponding to key "parameter" in "line"
+         * 
+         * @param line A string storing the line to be read
+         * @param parameter A string storing the key to be looked up
+         * @param space A bool indicating if there is spacing in the dataset that needs to be adjusted for
+         * @return An optional of double that stores the float of interest
+        */
         static std::optional<double> findDouble(std::string line, std::string parameter, bool space);
+
+        /**
+         * A static function that finds the JSON coordinates data corresponding to key "parameter" in "line"
+         * 
+         * @param line A string storing the line to be read
+         * @param parameter A string storing the key to be looked up
+         * @param space A bool indicating if there is spacing in the dataset that needs to be adjusted for
+         * @return An optional of list of pairs od doubles that stores the singed GPS coordinates of interest in format {longitude, latitude}
+        */
         static std::optional<std::list<std::pair<double, double>>> findCoordinatesList(std::string line, std::string parameter, unsigned int dim, bool space);
 };
--- data/utils.h
+++ data/utils.h
@@ -3,29 +3,82 @@
 #include <string>
 #include <list>
 
+/**
+ * ReaderUtils class
+*/
 class ReaderUtils {
     public:
+        /**
+         * RoadEntry struct
+        */
         struct RoadEntry {
+            /**
+             * Internal representation of a RoadEntry's name, length, and position
+             * 
+             * "name" is a string that stores the name of the road
+             * "length" is a double that stores the length of the road
+             * "coordinates_list" is a list of pairs of doubles that describes the road by a list of sequential signed GPS coordinates that lie on the road in the format {longitude, latitude}
+            */
             std::string name;
             double length;
             std::list<std::pair<double, double>> coordinates_list;
 
+            /**
+             * Defaulted empty constructor for RoadEntry
+            */
             RoadEntry();
+
+            /**
+             * Constructor for RoadEntry with name "argName", length "argLength", and coordinates "argCoordsList"
+            */
             RoadEntry(std::string argName, double argLength, std::list<std::pair<double, double>> argCoordsList);
         };
+        /**
+         * TrafficEntry struct
+        */
         struct TrafficEntry {
+            /**
+             * Internal representation of a TrafficEntry's name, traffic fequency, and position
+             * 
+             * "name" is a string that stores the name of the road
+             * "traffic" is an unsigned int that stores the daily traffic fequency on the road
+             * "coordinates_list" is a list of pairs of doubles that describes the road by a list of sequential signed GPS coordinates that lie on the road in the format {longitide, latitude}
+            */
             std::string road_name;
             unsigned int traffic;
             std::list<std::pair<double, double>> coordinates_list;
 
+            /**
+             * Defaulted empty constructor for TrafficEntry
+            */
             TrafficEntry();
+
+            /**
+             * Constructor for TrafficEntry with name "argName", traffic frequency "argTraffic", and coordinates "argCoordsList"
+            */
             TrafficEntry(std::string argName, unsigned int argTraffic, std::list<std::pair<double, double>> argCoordsList);
         };
+        /**
+         * CrashEntry struct
+        */
         struct CrashEntry {
+            /**
+             * Internal representation of a CrashEntry's crash fequency, and position
+             * 
+             * "vehicles" is an unsigned int that stores the number of vehicles involved in the crash
+             * "coordinates" is a pair of doubles that stores the signed GPS position of the crash in the format {longitude, latitude}
+            */
             unsigned int vehicles;
             std::pair<double, double> coordinates;
 
+            /**
+             * Defaulted empty constructor for CrashEntry
+            */
             CrashEntry();
+
+            /**
+             * Constructor for CrashEntry with number of vehicles involved "argVehicles" and coordinates "argCoords"
+            */
             CrashEntry(unsigned int argVehicles, std::pair<double, double> argCoords);
         };
 };
